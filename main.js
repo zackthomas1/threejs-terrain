1cc34781b9d7311f9692827ab5dc0d2fc3396000
zackthomas1 <zackthomas13@gmail.com>
2026-02-13 16:41:21 -0800

    created cube array and orbit controls on camera

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ class Application {
         this._renderer  = null;
         this._scene     = null;
         this._camera    = null;
-        this._cube      = null;
+        this._cubes      = null;
+        this._controls  = null;
         this._initialize();
     }
 
@@ -25,27 +26,63 @@ class Application {
         this._renderer.setAnimationLoop(() => this._animate());
         document.body.appendChild(this._renderer.domElement); // add renderer element to HTML document
 
+        // set up orbit controls
+        this._controls = new OrbitControls(this._camera, this._renderer.domElement);
+        this._controls.enableDamping = true;
+        this._controls.dampingFactor = 0.05;
+
         // Create geometry, apply a material to it, and insert into scene
         const geometry  = new THREE.BoxGeometry(1,1,1);
-        const material  = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        this._cube      = new THREE.Mesh(geometry, material);
-        this._scene.add(this._cube);
+        this._cubes     = [
+            this._makeGeometry(geometry, 0x44aa88, 0),
+            this._makeGeometry(geometry, 0x8844aa, -2),
+            this._makeGeometry(geometry, 0xaa8844, 2),
+        ];
+        
+        this._cubes.forEach((cube) => {
+            this._scene.add(cube);
+        });
+
+        // create a light 
+        const light = new THREE.DirectionalLight(0xFFFFFF, 3);  // color, intensity
+        light.position.set(-1,2,4);
+        this._scene.add(light);
+
+        // set scene background
+        this._scene.background = new THREE.Color(0x666666);
 
         // Position camera
         this._camera.position.z = 5;
     }
 
     _animate() {
-        // animate cube 
-        this._cube.rotation.x += 0.01;
-        this._cube.rotation.y += 0.01;
+        // update controls
+        this._controls.update();
+
+        // animate cube
+
+        this._cubes.forEach((cube, ndx) => {
+            const rot = (1 + ndx) * 0.01;
+            cube.rotation.x += rot;
+            cube.rotation.y += rot;
+        })
+
 
         this._renderer.render(this._scene, this._camera);
     }
+
+    _makeGeometry(geometry, color, xpos) { 
+        const material  = new THREE.MeshPhongMaterial({color});
+        const cube      = new THREE.Mesh(geometry, material);
+        cube.position.x = xpos;
+        return cube;
+    }
+
 }
 
+
 function _Main() {
-    _APP = new Application();
+    const app = new Application();
 }
 
 _Main();
