a3a4a183e04dbca2e8272d7a229b626f1ba2db90
zackthomas1 <zackthomas13@gmail.com>
2026-02-13 16:05:38 -0800

    Refactor application structure and add CSS styling

--- main.js
+++ main.js
@@ -5,29 +5,47 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 // import/create loader
 // const loader    = new GLTFLoader();
 
-// set up scene and camera
-const scene     = new THREE.Scene();
-const camera    = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+class Application {
+    constructor() {
+        this._renderer  = null;
+        this._scene     = null;
+        this._camera    = null;
+        this._cube      = null;
+        this._initialize();
+    }
 
-// set up renderer
-const renderer  = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement); // add renderer element to HTML document
+    _initialize() {
+        // set up scene and camera
+        this._scene     = new THREE.Scene();
+        this._camera    = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    
+        // set up renderer
+        this._renderer  = new THREE.WebGLRenderer();
+        this._renderer.setSize(window.innerWidth, window.innerHeight);
+        this._renderer.setAnimationLoop(() => this._animate());
+        document.body.appendChild(this._renderer.domElement); // add renderer element to HTML document
 
-// Create geometry, apply a material to it, and insert into scene
-const geometry = new THREE.BoxGeometry(1,1,1); 
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube      = new THREE.Mesh(geometry, material); 
-scene.add(cube);
+        // Create geometry, apply a material to it, and insert into scene
+        const geometry  = new THREE.BoxGeometry(1,1,1);
+        const material  = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        this._cube      = new THREE.Mesh(geometry, material);
+        this._scene.add(this._cube);
 
-// const controls  = new OrbitControls( camera, renderer.domElement );
-camera.position.z = 5;
+        // Position camera
+        this._camera.position.z = 5;
+    }
 
-function animate() {
-    // animate cube 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    _animate() {
+        // animate cube 
+        this._cube.rotation.x += 0.01;
+        this._cube.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
+        this._renderer.render(this._scene, this._camera);
+    }
 }
+
+function _Main() {
+    _APP = new Application();
+}
+
+_Main();
